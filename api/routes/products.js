9ad7fb4c225759118afa9ab353e6c7e70e97d95f
unknown <sang.kim37.ks47@gmail.com>
2019-04-26 01:11:59 +0700

    First commit

---
+++ api/routes/products.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
+const ProductsController = require('../controllers/products');
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+
+cloudinary.config({
+    cloud_name: 'khmer-make',
+    api_key: '745767955329641',
+    api_secret: '9VJyZS9cexI-IMVVRcmoForgMP8'
+});
+
+const storage = cloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: "demo",
+    allowedFormats: ["jpg", "png"]
+});
+
+const upload = multer({ storage: storage });
+
+router.get("/", ProductsController.products_get_all);
+
+router.post("/", checkAuth, upload.single('productImage'), ProductsController.products_create_product);
+
+router.get("/:productId", ProductsController.products_get_product);
+
+router.patch("/:productId", checkAuth, ProductsController.products_update_product);
+
+router.delete("/:productId", checkAuth, ProductsController.products_delete);
+
+module.exports = router;
