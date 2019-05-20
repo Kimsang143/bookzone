bbd8712da98ba06542aace59d7dab4a75ab6cb7b
unknown <sang.kim37.ks47@gmail.com>
2019-05-20 14:52:59 +0700

    fix date model

--- api/models/order.js
+++ api/models/order.js
@@ -4,6 +4,7 @@ const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
     quantity: { type: Number, default: 1 }
-});
+    },
+  	{timestamps: true });
 
 module.exports = mongoose.model('Order', orderSchema);
