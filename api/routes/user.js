27f2b2363568a9412bdc0ed3f612c9fa3e727ae3
JimJim143 <30286387+Kimsang143@users.noreply.github.com>
2019-05-30 13:20:15 +0700

    Update user.js

--- api/routes/user.js
+++ api/routes/user.js
@@ -10,7 +10,7 @@ router.post("/login", UserController.user_login);
 
 router.delete("/:userId", checkAuth, UserController.user_delete);
 
-router.get("/products/:productUser",checkAuth ,UserController.users_get_product);
+router.get("/products/:productUser",UserController.users_get_product);
 
 router.get("/",checkAuth ,UserController.users_get_all);
 
